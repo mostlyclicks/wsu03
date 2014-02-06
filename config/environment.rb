760b2bc49a21c3193d58897036cfbbf010e146c7
Carlos Interone <carlos@mostlyclicks.com>
2014-02-06 09:13:17 -0600

    initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+WSU03::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WSU03::Application.config.secret_key_base = 'e019da601b32f6dba2f805999a54776d6cbe8a9c68369fc68faf5b284d59f9ced3b005396b660d6d76886fe9fa7d99f28fb7ec3a9de8f67c9f9a138b641799e1'
