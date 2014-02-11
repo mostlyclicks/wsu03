3cc95ae39ae7eb751f15b8cc539b6f9eb3dc2df0
Carlos Interone <carlos@mostlyclicks.com>
2014-02-10 20:18:15 -0600

    add 3 versions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
+  layout 'application',  only: [:index, :tabs_working]
+  layout 'application_top', only: [:top_working]
+
   def index
+  end
+
+  def top_working
+
+  end
+
+  def tabs_working
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 WSU03::Application.routes.draw do
   get "pages/index"
+  get "pages/top_working"
+  get "pages/tabs_working"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
